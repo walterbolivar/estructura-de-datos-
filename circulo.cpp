9b33963d461344c95b3dfa0602c0415860cd1303
walter bolivar <junior_b10@hotmail.com>
2018-04-04 20:58:22 -0400

    Create circulo.cpp

---
+++ circulo.cpp
@@ -0,0 +1,24 @@
+#include "StdAfx.h"
+#include "circuloo.h"
+
+
+circuloo::circuloo(void)
+{
+}
+
+
+circuloo::~circuloo(void)
+{
+}
+float circuloo::calcularperimetro(float r)
+{
+	float pi=3.14,p;
+	      p=2*pi*r;
+		  return p;
+}
+float circuloo::calculararea(float r)
+{ float pi=3.14,a;
+        a=pi*r*r;
+		return a;
+
+}
